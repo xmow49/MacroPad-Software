7ed42167fce5cea13966b40f677afb433f681c64
xmow49 <47485034+xmow49@users.noreply.github.com>
2021-03-20 10:35:48 +0100

    add select media or keys

--- buttons-settings.js
+++ buttons-settings.js
@@ -18,30 +18,34 @@ function key1() {
 
   popupS.window({
     mode: 'confirm',
-    labelOk:     'Enregistrer',
+    labelOk: 'Enregistrer',
     labelCancel: 'Annuler',
     title: '',
     content: `<div class="dropper-form aligned">
                 <div>
-                  <input type="radio" id="key" name="key-media" value="key" onselect="selectKey()" checked>
+                  <input type="radio" id="key" name="key-media" value="keys" onclick="onChangeKeyType(this)" checked>
                   <label for="key">Combinaison de Touches</label>
-                  <input type="radio" id="media" name="key-media" value="media" onselect="selectMedia()">
+                  <input type="radio" id="media" name="key-media" value="media" onclick="onChangeKeyType(this)">
                   <label for="key">Controle des médias</label>
                 </div>
 
-                <div class="dropper-form-group" data-children-count="1">
-                <h3>Selection des touches:</h3>
+                <div id="key-div">
+                  <h3>Selection des touches:</h3>
                   <h4 id="key-combination"></h4>
                 </div>
+                <div id="media-div" hidden = true>
+                  <h3>Selection d'une action:</h3>
+                  <p>bonsoir</p>
+              </div>
               </div>`,
     onSubmit: function (val) {
       var txt = document.getElementById('key-combination').textContent;
-      if(txt == ""){
+      if (txt == "") {
         popupS.alert({
           content: "Merci d'entrer une combinaison de touches."
-      });
-      }else{
-        
+        });
+      } else {
+
       }
     },
     onClose: function () {
@@ -50,10 +54,22 @@ function key1() {
 
 }
 
-function selectMedia(){
+function selectMedia() {
   console.log("media")
 }
 
-function selectKey(){
+function selectKey() {
   console.log("key")
+}
+
+function onChangeKeyType(keymedia) {
+  var currentValue = keymedia.value;
+  console.log(keymedia.value);
+  if (currentValue == "keys") {
+    document.getElementById("key-div").hidden = false;
+    document.getElementById("media-div").hidden = true;
+  } else if (currentValue == "media") {
+    document.getElementById("key-div").hidden = true;
+    document.getElementById("media-div").hidden = false;
+  }
 }
