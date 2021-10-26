8b5d54d272366f85afa56447aafff866849293c0
xmow49 <47485034+xmow49@users.noreply.github.com>
2021-10-26 11:53:23 +0200

    update popup

--- src/windows/main/main.js
+++ src/windows/main/main.js
@@ -1,11 +1,10 @@
 //const { ipcMain, app, BrowserWindow, remote } = require("electron");
-const { remote } = require('electron')
 const { ipcRenderer } = require('electron')
 
+//------------------ Windows Buttons -------------------
 function closeBtn() {
     window.close();
 }
-
 
 function minimizeWindowBtn() {
     ipcRenderer.send("maximize");
@@ -14,3 +13,31 @@ function minimizeWindowBtn() {
 function minimizeBtn() {
     ipcRenderer.send("minimize");
 }
+//------------------ /Windows Buttons -------------------
+
+
+
+//------------------ Update Popup -------------------
+function updateAvailable() {
+    return true;
+}
+
+var updatePopup = document.getElementById("update-available");
+
+if (updateAvailable()) {
+    updatePopup.style.display = "block";
+}
+
+function updatePopupChanglogBtn() {
+    ipcRenderer.send("openLink", "https://github.com/xmow49/MacroPad-Software/releases")
+}
+
+function updatePopupSkipBtn() {
+    updatePopup.style.display = "none";
+}
+
+function updatePopupUpdateBtn() {
+
+}
+
+//------------------ /Update Popup -------------------
