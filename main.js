65d0969705bd6560d355be9901f70bb17bc2a5d7
Dorian Benech <dorian.benech007@gmail.com>
2022-03-08 18:28:38 +0100

    update event with dialogbox

--- main.js
+++ main.js
@@ -9,8 +9,8 @@ const fs = require('fs');
 const console = require('console');
 const { autoUpdater } = require("electron-updater")
 
-autoUpdater.logger = require("electron-log")
-autoUpdater.logger.transports.file.level = "info"
+
+
 
 let tray = null //background tray icon
 let mainWindow; //main window 
@@ -434,8 +434,8 @@ app.whenReady().then(() => {
     })
 
     updateAutoStart();
-    autoUpdater.checkForUpdatesAndNotify();
 
+    autoUpdater.checkForUpdates();
 })
 
 // explicitly with Cmd + Q.
@@ -461,3 +461,45 @@ function updateAutoStart() {
     }
 
 }
+
+
+let updater;
+
+
+autoUpdater.on('error', (error) => {
+    dialog.showErrorBox('Error: ', error == null ? "unknown" : (error.stack || error).toString())
+})
+
+autoUpdater.on('update-available', () => {
+    dialog.showMessageBox({
+        type: 'info',
+        title: 'Found Updates',
+        message: 'Found updates, do you want update now?',
+        buttons: ['Sure', 'No']
+    }).then((buttonIndex) => {
+        if (buttonIndex === 0) {
+            autoUpdater.downloadUpdate()
+        } else {
+            updater.enabled = true
+            updater = null
+        }
+    })
+})
+
+autoUpdater.on('update-not-available', () => {
+    dialog.showMessageBox({
+        title: 'No Updates',
+        message: 'Current version is up-to-date.'
+    })
+    updater.enabled = true
+    updater = null
+})
+
+autoUpdater.on('update-downloaded', () => {
+    dialog.showMessageBox({
+        title: 'Install Updates',
+        message: 'Updates downloaded, application will be quit for update...'
+    }).then(() => {
+        setImmediate(() => autoUpdater.quitAndInstall())
+    })
+})
